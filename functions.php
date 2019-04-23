20df14713eb93c6c7fbd24ec2b760f45b0fde93e
MB <wendigotech@gmail.com>
2019-04-23 15:16:37 +0200

    3

--- functions.php
+++ functions.php
@@ -227,14 +227,14 @@ if ( ! function_exists( 'Apr19_enqueue_scripts' ) ) :
     wp_deregister_style( 'style' );
     wp_enqueue_style( 'style', get_template_directory_uri() . '/dist/css/style.css', false, null, 'all');
 
-    wp_deregister_style( 'bootstrap' );
-    wp_enqueue_style( 'bootstrap', 'https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css', false, null, 'all');
-
     wp_deregister_style( 'all' );
     wp_enqueue_style( 'all', 'https://use.fontawesome.com/releases/v5.6.3/css/all.css', false, null, 'all');
 
     wp_deregister_style( 'custom' );
     wp_enqueue_style( 'custom', get_template_directory_uri() . '/custom.css', false, null, 'all');
+
+    wp_deregister_style( 'bootstrap' );
+    wp_enqueue_style( 'bootstrap', 'https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css', false, null, 'all');
 
     /* Pinegrow generated Enqueue Styles End */
 
@@ -247,8 +247,8 @@ function pgwp_sanitize_placeholder($input) { return $input; }
  * Resource files included by Pinegrow.
  */
 /* Pinegrow generated Include Resources Begin */
-require_once "inc/bootstrap/wp_bootstrap4_navwalker.php";
 require_once "inc/wp_smart_navwalker.php";
+require_once "inc/bootstrap/wp_bootstrap4_navwalker.php";
 
     /* Pinegrow generated Include Resources End */
 ?>
